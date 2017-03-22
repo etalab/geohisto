14fbba2b0d88623e602d497572d5a7d7bde744b0
David Larlet <david@larlet.fr>
2017-03-22 09:08:41 -0400

    Speed up tests for testing siblings, refs #25

--- tests/test_output_validity.py
+++ tests/test_output_validity.py
@@ -1,10 +1,12 @@
 """Tests related to output validity (duplicity, missing ancestors, etc)."""
+from itertools import groupby
 
 
 def test_unicity_per_boundaries(towns):
     """Ensure unicity for a given insee code at its start/end dates."""
-    for town_id, town in towns.items():
-        for sibling in towns.filter(depcom=town.depcom):
-            if sibling.id != town.id:
-                assert sibling.valid_at(town.start_datetime) is False
-                assert sibling.valid_at(town.end_datetime) is False
+    for depcom, siblings in groupby(towns.values(), lambda town: town.depcom):
+        for sibling1 in siblings:
+            for sibling2 in siblings:
+                if sibling1.id != sibling2.id:
+                    assert sibling1.valid_at(sibling2.start_datetime) is False
+                    assert sibling1.valid_at(sibling2.end_datetime) is False
